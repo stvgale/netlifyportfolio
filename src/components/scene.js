33c37de1f8aa145b7f1f675005b4fa7f0f89b40e
stvgale <stvgale@gmail.com>
2021-04-23 11:54:14 -0700

    Update scene.js

--- src/components/scene.js
+++ src/components/scene.js
@@ -16,19 +16,6 @@ class Scene extends React.Component {
     this.renderer.setSize(this.mount.offsetWidth, this.mount.offsetHeight)
     this.mount.appendChild(this.renderer.domElement)
 
-    const ambientLight = new THREE.AmbientLight();
-    scene.add(ambientLight);
-
-    const light = new THREE.DirectionalLight();
-    light.position.set(200, 100, 200);
-    light.castShadow = true;
-    light.shadow.camera.left = -100;
-    light.shadow.camera.right = 100;
-    light.shadow.camera.top = 100;
-    light.shadow.camera.bottom = -100;
-    scene.add(light);
-
-
     const geometry = new THREE.SphereGeometry(1.5, 3, 1);
     const material = new THREE.MeshNormalMaterial( {
 
@@ -46,7 +33,7 @@ class Scene extends React.Component {
   for (let i = 0; i < 500; i ++) {
     const material = new THREE.MeshPhongMaterial({
       color: colors[Math.floor(Math.random() * colors.length)],
-      shading: THREE.FlatShading
+      flatShading: THREE.FlatShading
     });
     const mesh = new THREE.Mesh(geometry, material);
     mesh.position.set((Math.random() - 0.5) * 1000,
